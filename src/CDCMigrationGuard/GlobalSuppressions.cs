f31c4d4d44eafff55d260167e2f3223d2c6d7720
Anuj Shroff <moonlight82@gmail.com>
2026-05-17 20:14:03 -0500

    Add update available notice

--- src/CDCMigrationGuard/GlobalSuppressions.cs
+++ src/CDCMigrationGuard/GlobalSuppressions.cs
@@ -9,3 +9,4 @@ using System.Diagnostics.CodeAnalysis;
 [assembly: SuppressMessage("Style", "IDE0130:Namespace does not match folder structure", Justification = "<Pending>", Scope = "namespace", Target = "~N:AnujShroff.CDCMigrationGuard.Models")]
 [assembly: SuppressMessage("Style", "IDE0130:Namespace does not match folder structure", Justification = "<Pending>", Scope = "namespace", Target = "~N:AnujShroff.CDCMigrationGuard.Output")]
 [assembly: SuppressMessage("Style", "IDE0130:Namespace does not match folder structure", Justification = "<Pending>", Scope = "namespace", Target = "~N:AnujShroff.CDCMigrationGuard.Services")]
+[assembly: SuppressMessage("Style", "IDE0130:Namespace does not match folder structure", Justification = "<Pending>", Scope = "namespace", Target = "~N:AnujShroff.CDCMigrationGuard")]
